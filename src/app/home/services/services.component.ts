80a9297fb46264e4613a75046fcf75a3d1cd185b
Dev-El-badry <miliarder96@gmail.com>
2020-03-22 20:10:34 +0200

    add animation to services

--- src/app/home/services/services.component.ts
+++ src/app/home/services/services.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-services',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onNavigate(url: string) {
+    console.log('here');
+    this.router.navigate(['/','insurance'], {queryParams: {page: url}}).then(res => {
+      window.scrollTo(0,0);
+    });
   }
 
 }
